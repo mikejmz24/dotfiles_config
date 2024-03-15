493f15baf4572b9357b7e3d21ef4ce29b953df06
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-03-15 13:41:59 -0700

    Corrected the .vscode & .config symlinks for the real files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,1 @@
+require("mikejmnz.core.options")
---
+++ .config/nvim/lua/mikejmnz/core/options.lua
@@ -0,0 +1,27 @@
+local opt = vim.opt
+
+-- line numbers
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+-- opt.ignorecase = true
+opt.smartcase = true
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
