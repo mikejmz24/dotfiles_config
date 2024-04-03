48cad5227728247936c4f39f1533d577bc6eff3b
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-04-02 18:56:34 -0700

    Updated Brewfile and added .gitignore for Zed tmp files

--- .config/nvim/lua/mikejmnz/plugins/colorscheme.lua
+++ .config/nvim/lua/mikejmnz/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-	-- {
-	--   "bluz71/vim-nightfly-guicolors",
-	--   priority = 1000, -- make sure to load this before all the other start plugins
-	--   config = function()
-	--     -- load the colorscheme here
-	--     vim.cmd([[colorscheme nightfly]])
-	--   end,
-	-- },
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("catppuccin").setup({
+	-- 			flavour = "frappe",
+	-- 			transparent_background = true,
+	-- 		})
+	-- 		vim.cmd.colorscheme("catppuccin")
+	-- 	end,
 	"folke/tokyonight.nvim",
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
