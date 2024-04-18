992cec6b4b510fa90dfac5f687a6efaee281abf7
mikejmnz24 <4935341+mikejmz24@users.noreply.github.com>
2024-04-18 13:14:47 -0700

    Added window management motion keymaps

--- .config/nvim/lua/mikejmnz/plugins/nvim-neotest.lua
+++ .config/nvim/lua/mikejmnz/plugins/nvim-neotest.lua
@@ -3,37 +3,40 @@ return {
 
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
+		-- "nvim-lua/plenary.nvim",
+		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
-		"nvim-neotest/nvim-nio",
 		"nvim-neotest/neotest-python",
-		"nvim-neotest/neotest-plenary",
+		-- "nvim-neotest/neotest-plenary",
+		-- "nvim-neotest/neotest-vim-test",
+		-- "mohammad5305/pytest.nvim",
 	},
 	config = function()
 		local neotest = require("neotest")
 		neotest.setup({
 			adapters = {
 				require("neotest-python")({
-					dap = { justMyCode = false },
+					-- dap = { justMyCode = false },
 					runner = "pytest",
 					pytest_discover_instances = true,
 				}),
-				require("neotest-plenary"),
+				-- 	require("neotest-plenary"),
 				-- 	require("neotest-vim-test")({
 				-- 		ignore_file_types = { "python", "vim", "lua" },
-				-- 	}),
+				-- 	},
 			},
 		})
 
 		-- Run the nearest test (Test current)
 		vim.keymap.set("n", "<leader>tc", function()
 			neotest.run.run()
-		end)
+		end, { desc = "Run current test" })
 
 		-- Run the current file (Test current file)
 		vim.keymap.set("n", "<leader>tC", function()
 			neotest.run.run(vim.fn.expand("%"))
-		end)
+		end, { desc = "Run current file" })
 	end,
 }
